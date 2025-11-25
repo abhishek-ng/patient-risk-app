6b43a39f5c050a23a9dc0d2cca66cede48550d44
abhishek-ng <abhishekngenux@gmail.com>
2025-11-25 15:07:44 +0530

    Update 1_upload_patient_data.py

--- pages/1_upload_patient_data.py
+++ pages/1_upload_patient_data.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import time
 
 st.set_page_config(page_title='Upload Patient Data')
 st.title('1. Upload Patient Data')
@@ -33,11 +32,7 @@ if uploaded is not None:
         if st.button("Save and Continue"):
             st.session_state["data_uploaded"] = True
 
-            with st.spinner("Saving data and preparing next step..."):
-                time.sleep(3)
-
-            st.toast("Redirecting to next step...", icon="➡️")
-            st.switch_page("2_clean_and_explore_data.py")
+            st.switch_page('2_clean_and_explore_data.py')
 
     except Exception as e:
         st.error("Failed to read file. Check format.")
@@ -64,4 +59,4 @@ if st.button("Load Demo Dataset"):
 
     st.session_state["raw_data"] = df
 
-    st.switch_page("2_clean_and_explore_data.py")
+    st.switch_page('2_clean_and_explore_data.py')
